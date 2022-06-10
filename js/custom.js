3977ef5b2b314db54c95ff277e3884eb609349a0
Michael Njogu <michael@openinstitute.com>
2022-06-10 10:10:41 +0300

    JS to change view on select change

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
     $('.data-table').DataTable({
         paging: false,
         searching: false,
-        select: true
+        select: true,
+        scrollX: true,
     });
 });
+
+$(function(){
+    // bind change event to select
+    $('#dynamic_select').on('change', function () {
+        var url = $(this).val(); // get selected value
+        if (url) { // require a URL
+            window.location = url; // redirect
+        }
+        return false;
+    });
+  });
